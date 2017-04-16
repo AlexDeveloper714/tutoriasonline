71a3581e1f45e79fe2bf936cb683f86fa97fbd11
AlexDeveloper714 <idiazcruz@uniminuto.edu.co>
2017-04-16 10:52:12 -0500

    Cambios en la pagina

---
+++ js/responsive_table.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#dataTables-example').DataTable({
+        responsive: true
+    });
+    $('#dataTables-example_2').DataTable({
+        responsive: true
+    });
+});
+
