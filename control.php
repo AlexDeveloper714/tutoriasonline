ddf4a4512f85a81303cf093d73bc114b3aa500e4
Alexander <Alexander@DESKTOP-VAB5N9A>
2017-04-16 11:15:13 -0500

    Cambios en la pagina

---
+++ control.php
@@ -0,0 +1,61 @@
+<?php
+require_once 'database.php';
+$db2 = new database();
+if (isset($_POST['enviarCliente'])) {
+    $db2->conectar();
+    $foto = $_FILES["file"]["name"];
+    $trozos = explode(".", $foto);
+    $exten = $_POST ['cedula'] . "." . end($trozos);
+    $ruta = $_FILES["file"]["tmp_name"];
+    $destino = "img/" . $foto;
+    $destino2 = "img/" . $exten;
+    copy($ruta, $destino);
+    rename($destino, $destino2);
+    if ($db2->verificarIdClientes($_POST ['cedula'], "clientes")) {
+        echo "<h1>
+           Cedula ya registrada, retrocediendo...
+       </h1>";
+        echo "<script type=\"text/javascript\">
+            setTimeout(function(){ history.go(-1); },500);
+       </script>";
+        exit;
+    } else {
+        $db2->insertar(array($_POST ['cedula'], $_POST['nombre'], $_POST['apellido'],
+            $_POST['genero'], $_POST ['fecha'], $_POST['correo'], $_POST['hijos'], $destino2), "clientes");
+        require 'index.php';
+    }
+}
+
+if (isset($_POST['enviarActivo'])) {
+    $db2->conectar();
+    $db2->insertarActivo(array(0, $_POST['tipoActivo'], $_POST['idCliente'],
+        $_POST['categoria'], $_POST ['marca'], $_POST['modelo'], $_POST['describir'], $_POST['notas']), "activos");
+    require 'index.php';
+}
+
+if (isset($_POST['actualizarActivo'])) {
+    $db2->conectar();
+    $db2->actualizarActivos(array($_POST['tipoActivo'],
+        $_POST['categoria'], $_POST ['marca'], $_POST['modelo'], $_POST['describir'], $_POST['notas']), "activos", $_POST ['idActivo']);
+    header("Location: datos_usuario.php?id_Cliente=" . $_POST['cedula']);
+}
+
+if (isset($_POST['actualizarDatos'])) {
+    $db2->conectar();
+    $foto = $_FILES["file"]["name"];
+    $trozos = explode(".", $foto);
+    $ruta = $_FILES["file"]["tmp_name"];
+    $exten = $_POST['cedula'] . "." . end($trozos);
+    $query = mysql_query("select ruta from clientes where id_cliente=" . $_POST['cedula']);
+    while ($row = mysql_fetch_array($query)) {
+        $anterior = $row[0];
+    }
+    $destino = "img/" . $foto;
+    $destino2 = "img/" . $exten;
+    $db2->actualizar(array($_POST['correo'], $_POST['hijos'], $destino2), "clientes", $_POST['cedula']);
+    unlink($anterior);
+    copy($ruta, $destino);
+    rename($destino, $destino2);
+    header("Location: datos_usuario.php?id_Cliente=" . $_POST['cedula']);
+}
+?>
---
+++ database.php
@@ -0,0 +1,79 @@
+<?php
+
+class database {
+
+    private $usuario;
+    private $password;
+    private $servidor;
+    private $nomDB;
+    private $link;
+
+    function database() {
+        $this->usuario = "root";
+        $this->password = "";
+        $this->servidor = "localhost";
+        $this->nomDB = "adminbienes";
+        $this->link = "";
+    }
+
+    function conectar() {
+        $this->link = mysql_connect($this->servidor, $this->usuario, $this->password);
+        mysql_select_db($this->nomDB, $this->link);
+    }
+
+    function insertar($fila = array(), $tabla = "") {
+        $valoresFila = "";
+        while (list($key, $val) = each($fila)) {
+            $valoresFila = $valoresFila . " '" . $val . "', ";
+        }
+        $valoresFila = substr($valoresFila, 0, -2);
+        mysql_query(" insert into " . $tabla . " values( " . $valoresFila . ");")or die("la consulta fallo (insertar)" . mysql_error());
+    }
+
+    function verificarIdClientes($idCliente, $tabla = "") {
+        $existe = false;
+        $query="select id_cliente from ".$tabla." where id_cliente=".$idCliente;
+        $existe=mysql_query($query);
+        return $existe;
+    }
+
+    function actualizarActivos($fila = array(), $tabla = "", $id_Activo = "") {
+        $actualizar = "update " . $tabla . " set tipo_activo='$fila[0]', "
+                . "categoria='$fila[1]', marca='$fila[2]', modelo='$fila[3]',"
+                . " descripcion='$fila[4]', notas='$fila[5]' where id_activos='$id_Activo'";
+        $res = mysql_query($actualizar)or die("la consulta fallo (insertar)" . mysql_error());
+        return $res;
+    }
+
+    function actualizar($fila = array(), $tabla = "", $idCliente = "") {
+        $actualizar = "update " . $tabla . " set correo='$fila[0]', num_hijos='$fila[1]', ruta='$fila[2]' where id_cliente='$idCliente'";
+        mysql_query($actualizar) and mysql_info();
+    }
+
+    function insertarActivo($fila = array(), $tabla = "") {
+        $valoresFila = "";
+        while (list($key, $val) = each($fila)) {
+            $valoresFila = $valoresFila . " '" . $val . "', ";
+        }
+        $valoresFila = substr($valoresFila, 0, -2);
+//quitar en sql_error($link) si no es link + $this-> link
+        mysql_query(" insert into " . $tabla . " values( " . $valoresFila . ");")or die("la consulta fallo(insertarActivo)" . mysql_error());
+    }
+
+    function consultarDB($tabla = "", $campo = "", $dato = "", $campoEspeci = "") {
+        if ($campo == "") {
+            $query = "select * from " . $tabla;
+        } else if ($dato == "") {
+            $query = "select " . $campo . " from " . $tabla;
+        } else if ($campoEspeci == "") {
+            $query = "select * from " . $tabla . " where " . $campo . " = " . $dato;
+        } else {
+            $query = "select " . $campoEspeci . " from " . $tabla . " where " . $campo . " = " . $dato;
+        }
+        $res = mysql_query($query);
+        return $res;
+    }
+
+}
+
+?>
