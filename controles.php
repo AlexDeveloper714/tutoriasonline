4f5b16ad96fdc2cc9743efbe7da0402591970531
Alexander <idiazcruz@uniminuto.edu.co>
2017-05-11 20:35:32 -0500

    Cambios CONTROL

--- controles.php
+++ controles.php
@@ -1,49 +1,24 @@
 <?php
-//$_SESSION (Permanente si las paginas que tienen session la inician)
 //Revisar que las tablas divisoras (m*m) sirvan
 @session_start();
 require_once 'database.php';
 $db2 = new database();
-//Insertar y verificar Clientes
-if (isset($_POST['enviarCliente'])) {
-    $db2->conectarDB();
-    if ($db2->verificarClientes($_POST ['documento'], "clientes") > 0) {
-        header("Location: errores.php");
-    } else {
-    $datosReq = [0, $_POST ['nombre'], $_POST['apellido'],$_POST['edad'], $_POST['tipoDocumento'],
-            $_POST['documento'], $_POST ['correo'], $_POST['universidad']
-            , $_POST['telefono'], $_POST ['tipoCliente']
-            , $_POST['usuario'], "ACTIVO", $_POST['clave']];
-        $db2->insertarDatos($datosReq, "clientes");
-    }
-    require 'index.php';
-}
-//Cambiar datos Usuario
-if (isset($_POST['hacerCambios'])) {
-    $db2->conectarDB();
-    $datosReq = [$_POST ['nombre'], $_POST['apellido'],$_POST['edad'],$_POST['tipoDocumento'],$_POST['documento'],$_POST['tipoUsuario'],$_POST['correo'],$_POST['telefono'],$_POST['clave']];
-    $camposReq = ["nombre", "apellido","edad","tipoDocumento","documento","tipoCliente","correo","telefono","clave"];
-    $datosBusq = [$_SESSION['usuario'],$_SESSION['tipoUsuario']];
-    $camposBus = ["usuario","tipoCliente"];
-    $db2->actualizarDatos($camposReq, $datosReq, $camposBus, $datosBusq, "clientes");
-    require 'perfil.php';
-}
-//Loguearse
+//Loguearse OK
 if (isset($_POST['entrarSistema'])) {
     $db2->conectarDB();
     $camposReq = ["*"];
     $datosBus = [$_POST ['usuario'], $_POST['clave'], "ACTIVO"];
     $camposBus = ["usuario", "clave", "estadoUsuario"];
     $db2->seleccionDatos($camposReq, $camposBus, $datosBus, "clientes");
-    if($_SESSION["usuario"]!=""){
+    if ($_SESSION["usuario"] != "") {
         require 'perfil.php';
-    }else{
+    } else {
         $_SESSION['usuario'] = "";
         $_SESSION['tipoUsuario'] = "";
         header("Location: errores.php");
     }
 }
-//Cerrar sesion
+//Cerrar sesion OK
 if (isset($_POST['salirSistema'])) {
     if ($_SESSION['usuario'] != "") {
         $_SESSION['usuario'] = "";
@@ -51,7 +26,21 @@ if (isset($_POST['salirSistema'])) {
     }
     require 'index.php';
 }
-//Insertar y verificar tutorias
+//Insertar y verificar Clientes OK
+if (isset($_POST['enviarCliente'])) {
+    $db2->conectarDB();
+    if ($db2->verificarClientes($_POST ['documento'], "clientes") > 0) {
+        header("Location: errores.php");
+    } else {
+        $datosReq = [0, $_POST ['nombre'], $_POST['apellido'], $_POST['edad'], $_POST['tipoDocumento'],
+            $_POST['documento'], $_POST ['correo'], $_POST['universidad']
+            , $_POST['telefono'], $_POST ['tipoCliente']
+            , $_POST['usuario'], "ACTIVO", $_POST['clave']];
+        $db2->insertarDatos($datosReq, "clientes");
+    }
+    require 'index.php';
+}
+//Insertar y verificar tutorias 1/2
 if (1 == 2) {
     $db2->conectarDB();
     //Cambiar condiciones
@@ -67,7 +56,17 @@ if (1 == 2) {
         
     }
 }
-//Actualizar datos Tutorias
+//Actualizar datos Usuario OK
+if (isset($_POST['hacerCambios'])) {
+    $db2->conectarDB();
+    $datosReq = [$_POST ['nombre'], $_POST['apellido'], $_POST['edad'], $_POST['tipoDocumento'], $_POST['documento'], $_POST['tipoUsuario'], $_POST['correo'], $_POST['telefono'], $_POST['clave']];
+    $camposReq = ["nombre", "apellido", "edad", "tipoDocumento", "documento", "tipoCliente", "correo", "telefono", "clave"];
+    $datosBusq = [$_SESSION['usuario'], $_SESSION['tipoUsuario']];
+    $camposBus = ["usuario", "tipoCliente"];
+    $db2->actualizarDatos($camposReq, $datosReq, $camposBus, $datosBusq, "clientes");
+    require 'perfil.php';
+}
+//Actualizar datos Tutorias 1/2
 if (1 == 2) {
     $db2->conectarDB();
     //Cambiar condiciones
